9c55131bb2d4c37501d130df06ba51fb73314129
Luke Edwards <luke.edwards05@gmail.com>
2021-01-31 19:53:07 -0800

    chore(utils): import `keys.until` helper

--- src/utils/keys.ts
+++ src/utils/keys.ts
@@ -9,3 +9,14 @@ export function format<K extends keyof Identifiers>(type: K, uid: Identifiers[K]
 export function gen<N extends number>(len: N): Fixed.String<N> {
 	return uid(len) as Fixed.String<N>;
 }
+
+export async function until<X extends string>(
+	toMake: () => X,
+	toSearch: (val: X) => Promise<unknown | false>
+): Promise<X> {
+	let exists, tmp = '' as X;
+	while (exists !== false) {
+		exists = await toSearch(tmp = toMake());
+	}
+	return tmp;
+}
