697949a8589ef03accdaac75995361a61cee349f
Luke Edwards <luke.edwards05@gmail.com>
2021-01-31 19:47:02 -0800

    chore: setup typescript worker

---
+++ @types/index.d.ts
@@ -0,0 +1,5 @@
+type TODO = any;
+
+type Nullable<T> = T | null;
+type Arrayable<T> = T | T[];
+type Promisable<T> = T | Promise<T>;
---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+
+const isDev = !!process.env.ROLLUP_WATCH;
+
+/**
+ * @type {import('rollup').RollupWatchOptions}
+ */
+const config = {
+	input: 'src/index.ts',
+	output: {
+		format: 'esm',
+		file: 'build/index.js',
+		sourcemap: isDev,
+	},
+	plugins: [
+		resolve({
+			mainFields: ['worker', 'browser', 'module', 'jsnext', 'main']
+		}),
+		typescript({
+			//
+		})
+	],
+	watch: {
+		clearScreen: false
+	}
+}
+
+export default config;
