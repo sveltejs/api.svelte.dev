e3135a826a2723fecb7a551042a0a237a095548a
Luke Edwards <luke.edwards05@gmail.com>
2021-01-31 20:02:00 -0800

    feat(session): add `identify` middleware method;
    - converts `req.headers.cookie` -> Session? -> User?

--- src/models/session.ts
+++ src/models/session.ts
@@ -1,6 +1,12 @@
+import * as User from './user';
+import * as database from '../utils/database';
+import * as cookies from '../utils/cookies';
 import * as keys from '../utils/keys';
 
+// types
 import type { UserID } from './user';
+import type { ServerResponse } from 'worktop/response';
+import type { ServerRequest } from 'worktop/request';
 
 export type SessionID = Fixed.String<32>;
 
@@ -16,4 +22,28 @@ export const toUID = () => keys.gen(32);
 /** Find a Session by its public ID value */
 export async function lookup(uid: SessionID) {
 	return database.get('session', uid);
+}
+
+/** Parse the "Cookie" request header; attempt valid `Session` -> `User` lookup */
+export async function identify(req: ServerRequest, res: ServerResponse): Promise<User.User | void> {
+	const cookie = req.headers.get('cookie');
+	if (!cookie) return res.send(401, 'Missing cookie header');
+
+	const sid = cookies.parse(cookie);
+	if (!sid) return res.send(401, 'Invalid cookie header');
+
+	const session = await lookup(sid);
+	if (!session) return res.send(401, 'Invalid cookie token');
+
+	if (Date.now() >= session.expires) {
+		// TODO: remove KV record
+		return res.send(401, 'Expired session');
+	}
+
+	const user = await User.lookup(session.userid);
+	if (!user || user.uid !== session.userid) {
+		return res.send(401, 'Invalid session');
+	}
+
+	return user;
 }
