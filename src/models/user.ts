c4dd06bcfbd51c5c0773ec6b3495a0c0ff74333a
Luke Edwards <luke.edwards05@gmail.com>
2021-01-31 19:57:27 -0800

    feat(user): add `gists` method via "owner" relation

--- src/models/user.ts
+++ src/models/user.ts
@@ -24,3 +24,8 @@ export type UserGist = Pick<Gist, 'uid'|'name'|'updated_at'>;
 export async function lookup(uid: UserID) {
 	return database.get('user', uid);
 }
+
+/** Get all Gists belonging to UserID */
+export function gists(uid: UserID): Promise<UserGist[]> {
+	return database.get('owner', uid).then(arr => arr || []);
+}
