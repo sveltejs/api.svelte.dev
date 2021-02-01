99620d91cc151c9d5c46b3926ba34c29019e7267
Luke Edwards <luke.edwards05@gmail.com>
2021-01-31 19:52:29 -0800

    feat(utils): add `keys.gen` helper for fixed UID lengths

--- src/utils/keys.ts
+++ src/utils/keys.ts
@@ -1,5 +1,11 @@
+import { uid } from 'uid';
+
 import type { Identifiers } from './database';
 
 export function format<K extends keyof Identifiers>(type: K, uid: Identifiers[K]) {
 	return `${type}__${uid}`;
+}
+
+export function gen<N extends number>(len: N): Fixed.String<N> {
+	return uid(len) as Fixed.String<N>;
 }
