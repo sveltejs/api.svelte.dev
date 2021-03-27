d9e000be37115480c019a5e20767ac5141745d73
Luke Edwards <luke.edwards05@gmail.com>
2021-03-27 12:44:39 -0700

    chore: update `cfw` version

--- cfw.js
+++ cfw.js
@@ -1,8 +1,12 @@
 const VARS = process.env;
 
+/**
+ * @type {import('cfw').Config}
+ */
 module.exports = {
-	name: 'svelte-api',
 	profile: 'svelte',
+	name: 'svelte-api',
+	entry: 'index.ts',
 	routes: [
 		'api.svelte.dev/*'
 	],
@@ -10,18 +14,5 @@ module.exports = {
 		DATAB: `KV:${VARS.CLOUDFLARE_NAMESPACEID}`,
 		GITHUB_CLIENT_ID: `ENV:${VARS.GITHUB_CLIENT_ID}`,
 		GITHUB_CLIENT_SECRET: `SECRET:${VARS.GITHUB_CLIENT_SECRET}`,
-	},
-	build(config, options) {
-		// Attach TypeScript config changes
-		// NOTICE: All this will be `cfw` built-in
-		config.input = config.input.replace(/\.js$/, '.ts');
-		options.resolve.extensions = ['.ts', '.mjs', '.js', '.json'];
-
-		config.plugins.push(
-			// @ts-ignore - require/default
-			require('rollup-plugin-typescript2')(),
-			// @ts-ignore - require/default
-			require('@rollup/plugin-commonjs')(),
-		);
 	}
 }
