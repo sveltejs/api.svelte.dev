eeacbd2168785350df06a8c54f8e1e393eb81d02
pngwn <hello@pngwn.io>
2021-04-18 18:59:29 +0100

    chore: fix formatting

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
-import { Router } from "worktop";
-import * as Cache from "worktop/cache";
-import * as Gists from "./routes/gists";
-import * as Todos from "./routes/todos";
-import * as Auth from "./routes/auth";
-import * as Docs from "./routes/docs";
+import { Router } from 'worktop';
+import * as Cache from 'worktop/cache';
+import * as Gists from './routes/gists';
+import * as Todos from './routes/todos';
+import * as Auth from './routes/auth';
+import * as Docs from './routes/docs';
 
 const API = new Router();
 
-API.add("GET", "/auth/login", Auth.login);
-API.add("GET", "/auth/callback", Auth.callback);
-API.add("GET", "/auth/logout", Auth.logout);
+API.add('GET', '/auth/login', Auth.login);
+API.add('GET', '/auth/callback', Auth.callback);
+API.add('GET', '/auth/logout', Auth.logout);
 
-API.add("GET", "/gists", Gists.list);
-API.add("POST", "/gists", Gists.create);
-API.add("GET", "/gists/:uid", Gists.show);
-API.add("PUT", "/gists/:uid", Gists.update);
-API.add("DELETE", "/gists/:uid", Gists.destroy);
+API.add('GET', '/gists', Gists.list);
+API.add('POST', '/gists', Gists.create);
+API.add('GET', '/gists/:uid', Gists.show);
+API.add('PUT', '/gists/:uid', Gists.update);
+API.add('DELETE', '/gists/:uid', Gists.destroy);
 
-API.add("GET", "/todos/:userid", Todos.list);
-API.add("POST", "/todos/:userid", Todos.create);
-API.add("PATCH", "/todos/:userid/:uid", Todos.update);
-API.add("DELETE", "/todos/:userid/:uid", Todos.destroy);
+API.add('GET', '/todos/:userid', Todos.list);
+API.add('POST', '/todos/:userid', Todos.create);
+API.add('PATCH', '/todos/:userid/:uid', Todos.update);
+API.add('DELETE', '/todos/:userid/:uid', Todos.destroy);
 
-API.add("GET", "/docs/:project/:type", Docs.list);
-API.add("GET", "/docs/:project/:type/:slug", Docs.entry);
+API.add('GET', '/docs/:project/:type', Docs.list);
+API.add('GET', '/docs/:project/:type/:slug', Docs.entry);
 
 Cache.listen(API.run);
