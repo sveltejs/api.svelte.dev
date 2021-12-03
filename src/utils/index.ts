ea20eaf7af2303d7025c0cba72193c1c8bef379e
Rich Harris <hello@rich-harris.dev>
2021-12-03 17:40:42 -0500

    dammit prettier

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import type { ServerResponse } from "worktop/response";
+import type { ServerResponse } from 'worktop/response';
 
 export function toError(res: ServerResponse, status: number, message: string) {
 	res.send(status, { status, message });
