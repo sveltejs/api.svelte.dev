be3f90b6a90d1cc66d3bdca80af5f7e0112b8308
Rich Harris <hello@rich-harris.dev>
2021-12-03 17:40:22 -0500

    revert toError change

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,5 @@
 import type { ServerResponse } from "worktop/response";
 
-export function toError(
-	res: ServerResponse,
-	status: number,
-	message: string,
-	headers: Record<string, string> = {}
-) {
-	for (const key in headers) {
-		res.setHeader(key, headers[key]);
-	}
-
+export function toError(res: ServerResponse, status: number, message: string) {
 	res.send(status, { status, message });
 }
