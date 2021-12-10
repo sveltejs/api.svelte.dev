4b76fb4e47e70fd4ae7804d76acdcb960fb573b5
Rich Harris <hello@rich-harris.dev>
2021-12-10 17:16:03 -0500

    remove session/gist stuff

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { Router } from 'worktop';
 import * as CORS from 'worktop/cors';
 import * as Cache from 'worktop/cache';
-import * as Gists from './routes/gists';
-import * as Sessions from './routes/sessions';
 import * as Todos from './routes/todos';
 import * as Docs from './routes/docs';
 
@@ -11,16 +9,6 @@ const API = new Router();
 API.prepare = CORS.preflight({
 	maxage: 3600
 });
-
-API.add('POST', '/session', Sessions.create);
-API.add('GET', '/session/:sessionid', Sessions.show);
-API.add('DELETE', '/session/:sessionid', Sessions.destroy);
-
-API.add('GET', '/gists', Gists.list);
-API.add('POST', '/gists', Gists.create);
-API.add('GET', '/gists/:gistid', Gists.show);
-API.add('PUT', '/gists/:gistid', Gists.update);
-API.add('DELETE', '/gists/:gistid', Gists.destroy);
 
 API.add('GET', '/todos/:guestid', Todos.list);
 API.add('POST', '/todos/:guestid', Todos.create);
