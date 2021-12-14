bbfa0573035fe0d02d957d5a0682f28cbb22101c
Rich Harris <hello@rich-harris.dev>
2021-12-14 18:48:55 -0500

    remove some unused code

--- src/utils/keys.ts
+++ src/utils/keys.ts
@@ -1,15 +1,8 @@
-import { until } from 'worktop/kv';
 import { uid } from 'worktop/utils';
 import type { Identifiers } from './database';
 
 export function format<K extends keyof Identifiers>(type: K, uid: Identifiers[K]) {
 	return `${type}__${uid}`;
-}
-
-const get_uid = () => uid(36);
-
-export function unique_uid(fn: (id: string) => Promise<boolean>) {
-	return until(get_uid, fn);
 }
 
 export { uid };
